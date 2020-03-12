4758e880a812baae6fce74caf39db2c2fec71b27
baebaee <wjstkdqo002@naver.com>
2020-03-12 19:47:36 +0900

    2020-03-12 SSL적용

--- src/main/java/service/MemberServiceImpl.java
+++ src/main/java/service/MemberServiceImpl.java
@@ -95,7 +95,7 @@ public class MemberServiceImpl implements MemberService {
 		// 6자리 인증 코드 생성
 		rand = (int) (Math.random() * 899999) + 100000;
 		
-		String sender="01033543929";
+		String sender="01096580540";
 
 		// 인증 코드를 데이터베이스에 저장하는 코드는 생략했습니다.
 
@@ -107,7 +107,7 @@ public class MemberServiceImpl implements MemberService {
 		credsProvider.setCredentials(new AuthScope(hostname, 443, AuthScope.ANY_REALM),
 
 				// 청기와랩에 등록한 Application Id 와 API key 를 입력합니다.
-				new UsernamePasswordCredentials("sinsegae", "e4d70bc45f8711ea8dea0cc47a1fcfae"));
+				new UsernamePasswordCredentials("jcyy", "832390da643911ea986f0cc47a1fcfae"));
 
 		AuthCache authCache = new BasicAuthCache();
 		authCache.put(new HttpHost(hostname, 443, "https"), new BasicScheme());
